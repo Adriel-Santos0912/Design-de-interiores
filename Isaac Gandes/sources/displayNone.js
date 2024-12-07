0f9795a90714b53e571493f5cebb2f5695acf05d
Adriel-Santos0912 <dev.santos0912@gmail.com>
2024-12-07 16:50:44 -0300

    adicionado nova estilização do site

--- Isaac Gandes/sources/displayNone.js
+++ Isaac Gandes/sources/displayNone.js
@@ -3,7 +3,7 @@ const footer = document.querySelector('footer');
 
 function checkWidth(){
         // Verifica a altura do footer
-    if (window.innerWidth > 768) {
+    if (window.innerWidth > 820) {
         footer.style.display = 'grid'
     } else {
         footer.style.display = 'none'
