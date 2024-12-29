6a8178afd429ea0e83f51fb3b6581fb2290cbcbd
Adriel <Dev.santos0912@gmail.com>
2024-12-28 22:27:20 -0300

    new commit
    adicionado arquivos todos mudados

---
+++ sources/fixedElement.js
@@ -0,0 +1,28 @@
+const fixedElement = document.getElementById('fixedElement');
+const header = document.querySelector('header');
+const img = document.querySelector('#logo');
+const contact = document.querySelector('.btnContact');
+let trocaImg = document.getElementById("trocaDeLogo")
+
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 50) {
+        // Esconde o elemento quando o scroll ultrapassa 50px
+        fixedElement.classList.add('hidden');
+        header.classList.remove('header')
+        header.classList.add('headerSmall')
+        img.style.display= 'none'
+        contact.classList.remove('btnContact')
+        contact.classList.add('btnContactBlack')
+        trocaImg.innerHTML = "<img id='logoBlack' class='img-fluid' src='sources/img/logoHeaderBlack.png' alt=''>"
+      } else {
+        // Mostra o elemento novamente quando voltar ao topo
+        fixedElement.classList.remove('hidden');
+        header.classList.remove('headerSmall')
+        header.classList.add('rool')
+        header.style.backgroundColor = ''
+        img.style.display= 'inline'
+        contact.classList.add('btnContact')
+        contact.classList.remove('btnContactBlack')
+        trocaImg.innerHTML = "<img id='logo' class='img-fluid' src='sources/img/logoHeader.png' alt=''>"
+      }
+    });
---
+++ sources/transformWidthImg.js
@@ -0,0 +1,42 @@
+const pImagem = document.querySelector('.imagem1');
+const sImagem = document.querySelector('.imagem2');
+const clique = document.querySelector('#click');
+
+function verificarResolucao() {
+    if(pImagem){
+        pImagem.addEventListener('click', () => {
+            sImagem.classList.remove('show');
+            sImagem.classList.add('hidden');
+            clique.style.display = 'none';
+        });
+
+        pImagem.addEventListener('mouseout', () => {
+            sImagem.style.display = 'block';
+            clique.style.display = 'block';
+            setTimeout(() => {
+                sImagem.classList.remove('hidden');
+                sImagem.classList.add('show');
+            }, 0);
+        });
+    }
+    if(sImagem){
+        sImagem.addEventListener('click', () => {
+            pImagem.classList.remove('show');
+            pImagem.classList.add('hidden');
+            clique.style.display = 'none';
+        });
+        // Função para remover o efeito
+        sImagem.addEventListener('mouseout', () => {
+            pImagem.style.display = 'block';
+            clique.style.display = 'block';
+            setTimeout(() => {
+                pImagem.classList.remove('hidden');
+                pImagem.classList.add('show');
+            }, 0);
+        });
+    }
+}
+
+window.addEventListener('resize', verificarResolucao);
+
+verificarResolucao();
